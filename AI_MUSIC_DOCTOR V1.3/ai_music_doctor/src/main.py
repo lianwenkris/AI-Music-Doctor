87431ebffba84b0e7ebf4df9ac15ba08e6da7461
Kris <5866113+lianwenkris@users.noreply.github.com>
2026-05-14 17:32:54 -0400

    Update main.py

--- AI_MUSIC_DOCTOR V1.3/ai_music_doctor/src/main.py
+++ AI_MUSIC_DOCTOR V1.3/ai_music_doctor/src/main.py
@@ -1,6 +1,4 @@
-<uploaded_files github_repo=mixdoktorz-bit/AI-Music-Doctor>
-<file=AI_MUSIC_DOCTOR V1.3/ai_music_doctor/src/main.py>
-#!/usr/bin/env python3
+
 """
 AI Music Doctor - Main Entry Point
 Denoise The Future Inc. © 2026
@@ -17,6 +15,3 @@ from gui import run_app
 if __name__ == '__main__':
     run_app()
 
-</file>
-
-</uploaded_files>
